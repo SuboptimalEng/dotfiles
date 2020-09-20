1348ce694be3cb2c798a62307697bcbf8572aec0
suboptimaleng <suboptimaleng@gmail.com>
2020-09-20 13:31:11 -0700

    Add dotfiles.

---
+++ iterm2/.oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
@@ -0,0 +1,62 @@
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
+  # update keymap variable for the prompt
+  VI_KEYMAP=$KEYMAP
+
+  zle reset-prompt
+  zle -R
+}
+
+zle -N zle-keymap-select
+
+function vi-accept-line() {
+  VI_KEYMAP=main
+  zle accept-line
+}
+
+zle -N vi-accept-line
+
+export KEYTIMEOUT=25
+
+bindkey -v
+bindkey -M viins kj vi-cmd-mode
+
+# use custom accept-line widget to update $VI_KEYMAP
+bindkey -M vicmd '^J' vi-accept-line
+bindkey -M vicmd '^M' vi-accept-line
+
+# allow v to edit the command line (standard behaviour)
+# autoload -Uz edit-command-line
+# zle -N edit-command-line
+# bindkey -M vicmd 'v' edit-command-line
+
+# allow ctrl-p, ctrl-n for navigate history (standard b
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# allow ctrl-h, ctrl-w, ctrl-? for char and word deleti
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+
+# allow ctrl-r and ctrl-s to search the history
+# bindkey '^r' history-incremental-search-backward
+bindkey '/' history-incremental-search-backward
+bindkey '^s' history-incremental-search-forward
+# allow ctrl-a and ctrl-e to move to beginning/end of l
+bindkey '^a' beginning-of-line
+bindkey '^ ' end-of-line
+
+# if mode indicator wasn't setup by theme, define defau
+if [[ "$MODE_INDICATOR" == "" ]]; then
+  MODE_INDICATOR="%{$fg_bold[red]%}<%{$fg[red]%}<<%{$re
+fi
+
+function vi_mode_prompt_info() {
+  echo "${${VI_KEYMAP/vicmd/$MODE_INDICATOR}/(main|viin
+}
+
+# define right prompt, if it wasn't defined by a theme
+if [[ "$RPS1" == "" && "$RPROMPT" == "" ]]; then
+  RPS1='$(vi_mode_prompt_info)'
+fi
