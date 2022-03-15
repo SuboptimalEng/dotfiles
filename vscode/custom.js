ff852fbebb2719730a836deb5e55c6745838860e
Suboptimal Engineer <suboptimaleng@gmail.com>
2022-03-15 10:51:33 -0500

    Refactor trailing cursor code.

--- vscode/custom.js
+++ vscode/custom.js
@@ -6,53 +6,60 @@ function getOffset(el) {
   };
 }
 
-const displayEffect = (event) => {
-  setTimeout(function () {
-    let offset = getOffset(document.getElementsByClassName("cursor")[0]);
-    let top = offset.top;
-    let left = offset.left;
-    console.log(top, left);
-  }, 40);
+const prevOffset = {};
 
-  console.log(event);
-  let existingRect = document.getElementById("myRect");
+const displayEffect = (event) => {
+  if (prevOffset.top === undefined) {
+    let initialOffset = getOffset(document.getElementsByClassName("cursor")[0]);
+    prevOffset.top = initialOffset.top;
+    prevOffset.left = initialOffset.left;
+  }
+
+  const existingRect = document.getElementById("myRect");
   if (existingRect !== null) {
     existingRect.remove();
   }
 
-  let rect = document.createElement("div");
-  let offset = getOffset(document.getElementsByClassName("cursor")[0]);
-  let top = offset.top;
-  let left = offset.left;
-  console.log(top, left);
+  setTimeout(function () {
+    const currOffset = getOffset(document.getElementsByClassName("cursor")[0]);
+    const top = prevOffset.top;
+    const left = prevOffset.left;
+    const rect = document.createElement("div");
 
-  rect.id = "myRect";
-  rect.style.cssText = `
-	position:absolute;
-	top:${top}px;
-	left:${left}px;
-	width:9px;
-	height:20px;
-	z-index:10;
-	background-color:red;
-  opacity: 0.5;
-  `;
-  rect.animate(
-    [
-      // keyframes
-      { transform: "scale(1) skew(25deg, 10deg)" },
-      { transform: "scale(0) skew(25deg, 10deg)" },
-    ],
-    {
-      // timing options
-      duration: 1000,
-      easing: "ease-in-out",
-      direction: "alternate",
-      iterations: Infinity,
-    }
-  );
+    // console.log("prev: ", top, left);
+    // console.log("curr: ", currOffset.top, currOffset.left);
 
-  document.body.appendChild(rect);
+    rect.id = "myRect";
+    rect.style.cssText = `
+      position:absolute;
+      top:${top}px;
+      left:${left}px;
+      width:9px;
+      height:20px;
+      z-index:10;
+      background-color:red;
+      opacity: 0.5;
+    `;
+    rect.animate(
+      [
+        // keyframes
+        { transform: "scale(1)" },
+        { transform: "scale(0)" },
+      ],
+      {
+        // timing options
+        duration: 500,
+        easing: "ease-in-out",
+        direction: "alternate",
+        iterations: Infinity,
+      }
+    );
+
+    document.body.appendChild(rect);
+    prevOffset.top = currOffset.top;
+    prevOffset.left = currOffset.left;
+  }, 25);
 };
 
-document.addEventListener("keydown", displayEffect);
+// NOTE: Uncomment this to get a cursor trail effect.
+// document.addEventListener("keydown", displayEffect);
